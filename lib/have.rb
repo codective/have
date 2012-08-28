beebc378a41fa36e3b02f7947ee09b1f2bddf2e7
Sam Buna <codective@gmail.com>
2012-08-28 21:37:05 +0000

    support has_any? and has_all? that works on array elements too

--- lib/have.rb
+++ lib/have.rb
@@ -2,8 +2,20 @@ require "have/version"
 
 module Have
   def has? arg
-    result = self.send(arg)
-    result && result != ''
+    if respond_to? arg.to_s
+      result = self.send(arg)
+      result && result != ''
+    else
+      include? arg
+    end
+  end
+
+  def has_any? *args
+    args.any? { |arg| has? arg }
+  end
+
+  def has_all? *args
+    args.all? { |arg| has? arg }
   end
 end
 
--- spec/have_spec.rb
+++ spec/have_spec.rb
@@ -1,5 +1,5 @@
 class Tv
-  attr_accessor :hdmi
+  attr_accessor :hdmi, :active3d, :passive3d, :internet, :wireless
 end
 
 require 'have'
@@ -11,6 +11,38 @@ describe Have do
       tv.has?(:hdmi).should be_false
       tv.hdmi = 1
       tv.has?(:hdmi).should be_true
+      tv.hdmi = false
+      tv.has?(:hdmi).should be_false
+    end
+  end
+
+  describe '#has_any?' do
+    it 'works on values for an array receiver' do
+      [1,2,3].has_any?(1). should be_true
+      [1,2,3].has_any?(4). should be_false
+      [1,2,3].has_any?(2, 7). should be_true
+      [1,2,3].has_any?(6, 7). should be_false
+    end
+    it 'checks for the presence of any argument as a method on the receiver' do
+      tv.internet = true
+      tv.has_any?(:internet, :active3d). should be_true
+      tv.has_any?(:active3d, :passive3d). should be_false
+    end
+  end
+
+  describe '#has_all?' do
+    it 'works on values for an array receiver' do
+      [1,2,3].has_all?(1, 2). should be_true
+      [1,2,3].has_all?(1, 7). should be_false
+      [1,2,3].has_all?(6, 7). should be_false
+    end
+    it 'does not accont for order' do
+      [1,2,3].has_all?(2, 1). should be_true
+    end
+    it 'checks for the presence of all argument as a method on the receiver' do
+      tv.hdmi = tv.internet = tv.wireless = true
+      tv.has_all?(:internet, :wireless). should be_true
+      tv.has_all?(:wireless, :passive3d). should be_false
     end
   end
 end
